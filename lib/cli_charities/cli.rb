46ebbf222241f7b965939c517b294f39997fb2cb
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-24 10:52:25 -0700

    got rid of duplkicate folder

--- lib/cli_charities/cli.rb
+++ lib/cli_charities/cli.rb
@@ -2,7 +2,7 @@ class CLI
 
   def start
     puts "Hello, welcome to Charities CLI"
-    puts "Please enter Zip Code"
+    puts "Please enter a Zip Code"
     input = gets.chomp
     API.get_charity(input)
     
