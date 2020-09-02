c9e0639b7cdc7e01dc98e2a28790a5e2c294e630
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-09-02 01:35:21 -0700

    final touches

--- lib/cli_charities/api.rb
+++ lib/cli_charities/api.rb
@@ -1,8 +1,5 @@
 class API
   def self.get_charities(zipcode)
-    #include HTTParty
-    #base_uri = url
- # rescue
     key = "caa5b57e5c420e8a0cc13fdafca8e8b0"
     url = "http://data.orghunter.com/v1/charitysearch?user_key=caa5b57e5c420e8a0cc13fdafca8e8b0&zipCode=#{zipcode}"
     response = HTTParty.get(url)
